77fc02b4df659bea00efdc9c2d1693ff131241ea
Slangman3 <slangman3@gmail.com>
2019-08-12 16:57:45 +0300

    Added to POM chat new selectors and methods.

--- src/test/java/pages/IntegriVideoChatPage.java
+++ src/test/java/pages/IntegriVideoChatPage.java
@@ -3,19 +3,25 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.mustache.Value;
+import tests.TestData;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.security.Key;
+import java.util.EmptyStackException;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.*;
 
-public class IntegriVideoChatPage {
+public class IntegriVideoChatPage extends TestData {
     private static final String URL = "https://dev.integrivideo.com/demo/chat/new";
     WebDriver driver;
+    WebDriverWait wait;
     By inviteLink = By.id("invite-users-to-chat");
     By inputArea = By.xpath("//*[@placeholder='Start typing here']");
     By sendMessageButton = By.xpath("//*[@title='Send message']");
@@ -23,10 +29,13 @@ public class IntegriVideoChatPage {
     By settings = By.cssSelector(".integri-chat-settings");
     By code = By.className("component-code");
     By messageText = By.className("integri-chat-message-text");
-
+    By messageWithLink = By.className("integri-chat-message-attachment-link");
+    By editBtn = By.className("integri-chat-edit-message");
+    By editArea = By.xpath("//*[@id='integri-component-chat']/div[4]/div[2]/div/div/div[2]/textarea");
 
     public IntegriVideoChatPage(WebDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
     }
 
     public void openPage() {
@@ -83,4 +92,34 @@ public class IntegriVideoChatPage {
         assertEquals(actual, expectedMessage);
     }
 
+    public void verifyVisibilityJSMessage() {
+        String JSCode = driver.findElement(messageText).getAttribute("innerText");
+        assertTrue(JSCode.isEmpty());
+    }
+
+    public void editClickAndReMessageOn(String text){
+        driver.findElement(editBtn).click();
+        driver.findElement(editArea).clear();
+        driver.findElement(editArea).sendKeys(text);
+        driver.findElement(editArea).sendKeys(Keys.ENTER);
+    }
+
+    public void verifyEditText(String expectedMessage) {
+        wait.until(ExpectedConditions.textToBe(By.className("integri-chat-message-text"), expectedMessage));
+        String actual = driver.findElement(messageText).getText();
+        assertEquals(actual, expectedMessage);
+    }
+
+    public void verifyMessageLinkDimaGoogleDisk(){
+        assertFalse(driver.findElements(messageWithLink).isEmpty());
+        String text = driver.findElement(By.className("integri-chat-message-attachment-link")).getAttribute("innerText");
+        assertEquals(text, "drive.google.com\n" +
+                "lesson_7_selenium_advanced.pptx - Google Drive");
+    }
+
+    public void verifyMessageLinkTutBy(){
+        assertFalse(driver.findElements(messageWithLink).isEmpty());
+        String text = driver.findElement(By.className("integri-chat-message-attachment-link")).getAttribute("innerText");
+        assertEquals(text, "undefined");
+    }
 }
