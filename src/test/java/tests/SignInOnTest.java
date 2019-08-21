048fc0640fd291324c6f2b8446dbf79e23a5f8da
Slangman3 <slangman3@gmail.com>
2019-08-21 17:28:03 +0300

    Added tests about verifying sign in page.

---
+++ src/test/java/tests/SignInOnTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+import pages.IntegriVideoChatLoginPageFactory;
+import pages.IntegriVideoChatSignInPage;
+
+public class SignInOnTest {
+    ChromeDriver driver;
+    IntegriVideoChatLoginPageFactory pageFactory;
+    IntegriVideoChatSignInPage signInPage;
+
+    @BeforeClass
+    public void openDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void signUpOnCheck() {
+        IntegriVideoChatLoginPageFactory pageFactory = new IntegriVideoChatLoginPageFactory(driver);
+        IntegriVideoChatSignInPage signInPage = new IntegriVideoChatSignInPage(driver);
+        pageFactory
+                .openPage()
+                .signUp();
+        signInPage.verifySignInPage();
+    }
+
+    @AfterClass
+    public void closeDriver() {
+        driver.quit();
+    }
+
+}
